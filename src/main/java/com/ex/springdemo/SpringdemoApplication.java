5924698cb0f819316e4c498a3662c5beb707b2af
Birudukota <2094140@cognizant.com>
2022-08-05 10:23:54 +0530

    commit for springdemo

---
+++ src/main/java/com/ex/springdemo/SpringdemoApplication.java
@@ -0,0 +1,32 @@
+package com.ex.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the bean provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+			
+		};
+	}
+
+}
